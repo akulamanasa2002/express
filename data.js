24934f775cbb627fbed0c812d28da6846a51fff5
Ramani-Narasimha <thunuguntlaramani@gmail.com>
2022-11-23 12:00:35 +0530

    express

---
+++ data.js
@@ -0,0 +1,7 @@
+const students=[
+	{stdid:1,stdname:"manasa",stdclass:"mpcs"},
+	{stdid:2,stdname:"lali",stdclass:"bzc"},
+	{stdid:3,stdname:"ramya",stdclass:"cams"},
+	{stdid:4,stdname:"sravan",stdclass:"mpc"},
+]
+module.exports=students
